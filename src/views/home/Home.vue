76a3fdca7aaeb063dee21c664b95ce334ba030a1
qianmo39 <qianmo39@outlook.com>
2022-10-28 00:04:56 +0800

    feat: 请求首页数据

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -8,8 +8,25 @@
 
 <script>
 import NavBar from 'components/common/navbar/NavBar';
+import { getHomeMultidata } from "network/home";
+
 export default {
   name: "Home",
+  data() {
+    return {
+      banners: [],
+      keywords: [],
+      recommends: []
+    }
+  },
+  created() {
+    getHomeMultidata().then(res => {
+      console.log(res.data);
+      this.banners = res.data.banner.list;
+      this.keywords = res.data.keywords.list;
+      this.recommends = res.data.recommend.list;
+    })
+  },
   components: {
     NavBar,
   }
