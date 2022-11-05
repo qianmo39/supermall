aa31d517660795d09a50b821452825331b01fdfa
qianmo39 <qianmo39@outlook.com>
2022-11-05 19:19:08 +0800

    feat: 详情页轮播图

--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -1,24 +1,33 @@
 <template>
   <div id="detail">
-    <detail-nav-bar></detail-nav-bar>
+    <detail-nav-bar />
+    <detail-swiper :topImgs="topImgs" />
   </div>
 </template>
 
 <script>
 import DetailNavBar from "./childComps/DetailNavBar";
+import DetailSwiper from "./childComps/DetailSwiper";
+import { getDetail } from "network/detail";
 
 export default {
   name: "Detail",
   data() {
     return {
       iid: null,
+      topImgs: [],
     };
   },
-  activated() {
+  created() {
     this.iid = this.$route.params.iid;
+    getDetail(this.iid).then((res) => {
+      console.log(res);
+      this.topImgs = res.result.itemInfo.topImages;
+    });
   },
   components: {
     DetailNavBar,
+    DetailSwiper,
   },
 };
 </script>
