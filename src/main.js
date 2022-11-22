03b0f200131b7e1b04d3f29aec9528608207b8cf
qianmo39 <qianmo39@outlook.com>
2022-11-22 14:08:28 +0800

    feat: lazyload

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import Vue from "vue";
-import FastClick from "fastclick";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import toast from "./components/common/toast";
+
+import FastClick from "fastclick";
+import VueLazyLoad from "vue-lazyload";
 
 Vue.config.productionTip = false;
 
@@ -12,6 +14,12 @@ Vue.prototype.$bus = new Vue();
 Vue.use(toast);
 
 FastClick.attach(document.body);
+
+const loadImage = require("./assets/img/common/placeholder.png");
+
+Vue.use(VueLazyLoad, {
+  loading: loadImage,
+});
 
 new Vue({
   render: (h) => h(App),
