d5ba9a0215460a25c9bca6c692a577686005ffc6
qianmo39 <qianmo39@outlook.com>
2022-11-22 13:48:46 +0800

    feat: fast click

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import Vue from "vue";
+import FastClick from "fastclick";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
@@ -9,6 +10,8 @@ Vue.config.productionTip = false;
 Vue.prototype.$bus = new Vue();
 
 Vue.use(toast);
+
+FastClick.attach(document.body);
 
 new Vue({
   render: (h) => h(App),
